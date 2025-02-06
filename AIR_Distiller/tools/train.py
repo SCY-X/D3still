9f75f30afca3d7df00ac4c320792ca0fd48d7ac9
SCY <38521156+SCY-X@users.noreply.github.com>
2025-02-06 21:10:31 +0800

    Update train.py

--- AIR_Distiller/tools/train.py
+++ AIR_Distiller/tools/train.py
@@ -8,7 +8,7 @@ import torch
 
 
 import sys
-sys.path.append(os.path.abspath("/home/data2/xieyi/D3_Distillation/AIR_KD"))
+sys.path.append(os.path.abspath("XXXXX/AIR_Distiller"))
 
 from config import cfg
 from utils.logger import setup_logger
