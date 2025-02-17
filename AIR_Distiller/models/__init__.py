ceb9f7982ba69d43245c9075a4689a80c0ea674f
SCY <38521156+SCY-X@users.noreply.github.com>
2025-02-17 23:54:31 +0800

    Update __init__.py

--- AIR_Distiller/models/__init__.py
+++ AIR_Distiller/models/__init__.py
@@ -1,5 +1,6 @@
 from .resnet import resnet18, resnet34, resnet50, resnet101, resnet152
 from .resnet_ibn import resnet18_ibn_a, resnet34_ibn_a, resnet50_ibn_a, resnet101_ibn_a
+from .swin_transformer_v2 import swin_transformer_v2_base, swin_transformer_v2_small, swin_transformer_v2_tiny
 from .mobilenetv3 import mobilenetv3_small
 
 
@@ -12,5 +13,8 @@ model_dict = {
     "ResNet34_ibn_a": resnet34_ibn_a,
     "ResNet50_ibn_a": resnet50_ibn_a,
     "ResNet101_ibn_a": resnet101_ibn_a,
-    "MobileNetV3_Small": mobilenetv3_small
+    "MobileNetV3_Small": mobilenetv3_small,
+    "Swin_Transformer_V2_Base": swin_transformer_v2_base,
+    "Swin_Transformer_V2_Small": swin_transformer_v2_small,
+    "Swin_Transformer_V2_Tiny": swin_transformer_v2_tiny
 }
